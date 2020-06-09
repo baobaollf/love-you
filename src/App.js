50213c5dfaaeed3a23fc3c73891ea23b5b176697
baobaollf <55776365+baobaollf@users.noreply.github.com>
2020-06-08 18:26:08 -0700

    added image

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Img from "../src/Image/IMG_0046.jpg"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Love you Sitong, from the moon and back
+      </h1>
+        <img className="image" src={Img}/>
     </div>
   );
 }
