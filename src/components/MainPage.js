94418cb84aba4da18d6749ae50228cdd908ebcff
Baobao <hjwllf@gmail.com>
2020-07-19 22:30:16 -0700

    adjest icon

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -5,30 +5,37 @@ import { Layout, Menu } from 'antd';
 const { Header, Content, Footer } = Layout;
 
 export default class MainPage extends Component {
+
+
     render() {
         return (
             <Layout className="layout">
                 <Header>
                     <img className="mainPageLogo" src={logo} alt={logo} />
 
-                    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-                        <Menu.Item key="1">nav 1</Menu.Item>
-                        <Menu.Item key="2">nav 2</Menu.Item>
-                        <Menu.Item key="3">nav 3</Menu.Item>
+                    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+                        <Menu.Item key="1">Home</Menu.Item>
+                        <Menu.Item key="2">Explore</Menu.Item>
+                        <Menu.Item key="3">About Us</Menu.Item>
                     </Menu>
                 </Header>
-                <Content style={{ padding: '0 50px' }}>
+                <Content style={{
+                    padding: '0 50px'
+                }}>
                     <div className="site-layout-content"><div className="App">
 
-                        <h1>
-                            Love you Sitong, Happy Birthday
-                </h1>
-                        <h1>
+                    <h1>
+                        Love you Sitong, Happy Birthday
+                    </h1>
+                    <h1>
                             Forever 18
-                </h1>
-                        <h5>
-                            Baobao
-	            </h5>
+                    </h1>
+                    <h6>
+                            7/19/2020
+                    </h6>
+                    <h5>
+                        Baobao
+	                </h5>
                         <img className="image" src={Img} alt={Img} />
                     </div></div>
                 </Content>
